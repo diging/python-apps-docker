e21341e32f9315b3f0c16025854a3ad124bc14e6
Taylor Quinn <tjquinn1@asu.edu>
2019-09-05 16:34:48 -0700

    added mbl dockerfile for production

---
+++ mbl/scripts/mbl-gunicorn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAME="mbl-browser"                                  # Name of the application
+DJANGODIR=/usr/src/app/mbl-browser            # Django project directory
+SOCKFILE=/usr/src/app/run/mbl.sock  # we will communicte using this unix socket
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=mbl.production_settings             # which settings file should Django use
+DJANGO_WSGI_MODULE=mbl.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source env_secrets
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --bind=0.0.0.0:8000 \
+  --log-level=debug \
+  --log-file=-
